2325416faa105292982b23cb97dd7976477fb54b
Paulo Victor <pvictor36@gmail.com>
2022-06-11 17:09:56 -0300

    Criando banco de dados loja de games e configurando aplication

---
+++ Atividades Programação/Bloco 2/backendgames/backendgames/src/main/java/com/lojadegames/backendgames/BackendLojaDeGamesApplication.java
@@ -0,0 +1,13 @@
+package com.lojadegames.backendgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendLojaDeGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendLojaDeGamesApplication.class, args);
+	}
+
+}
---
+++ Atividades Programação/Bloco 2/backendgames/backendgames/src/test/java/com/lojadegames/backendgames/BackendLojaDeGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lojadegames.backendgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLojaDeGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
