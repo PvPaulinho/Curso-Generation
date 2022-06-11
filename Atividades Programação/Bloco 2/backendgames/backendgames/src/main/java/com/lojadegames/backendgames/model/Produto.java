aa60bbb3385de4df6663a821da2de7663d734d13
Paulo Victor <pvictor36@gmail.com>
2022-06-11 17:18:32 -0300

    Criando model Produto

---
+++ Atividades Programação/Bloco 2/backendgames/backendgames/src/main/java/com/lojadegames/backendgames/model/Produto.java
@@ -0,0 +1,62 @@
+package com.lojadegames.backendgames.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy  = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 3, max = 20)
+	private String nome;
+	
+	@NotNull
+	private BigDecimal preco;
+	
+	@NotNull
+	private int quantidade;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+}
